eaf0d3df63a9d8054d3a4be3ffea08fd6126ebac
John-Ad <adriaanscorne@gmail.com>
2023-06-20 15:41:43 +0200

    updated home carousel to svelte one

--- app/src/views/HomeView.svelte
+++ app/src/views/HomeView.svelte
@@ -5,18 +5,36 @@
   import Pricing from "../sections/pricing/pricing.svelte";
   import Services from "../sections/services/services.svelte";
   import Team from "../sections/team/team.svelte";
+  import Carousel from "svelte-carousel";
+
+  let carousel; // for calling methods of the carousel instance
+
+  const handleSlideChangeClick = (prev: boolean) => {
+    if (prev) carousel.goToPrev();
+    else carousel.goToNext();
+  };
 </script>
 
 <div class="container-fluid p-0 mb-5">
-  <div class="owl-carousel header-carousel position-relative">
-    <div class="owl-carousel-item position-relative">
+  <Carousel
+    bind:this={carousel}
+    arrows={true}
+    infinite={true}
+    dots={false}
+    duration={2000}
+    autoplay={true}
+    autoplayDuration={7000}
+    class="intro wow fadeInUp"
+    data-wow-delay="0.6s"
+  >
+    <div class="intro-item position-relative">
       <img class="img-fluid" src="img/Image BG 1.jpg" alt="" />
       <div
         class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
         style="background: rgba(24, 29, 56, .7);"
       >
         <div class="container">
-          <div class="row justify-content-start">
+          <div class="row justify-content-center">
             <div class="col-sm-10 col-lg-8">
               <h5 class="text-primary text-uppercase mb-3 animated slideInDown">
                 Experience the Best Online Learning
@@ -46,14 +64,14 @@
         </div>
       </div>
     </div>
-    <div class="owl-carousel-item position-relative">
+    <div class="intro-item position-relative w-100">
       <img class="img-fluid" src="img/Image BG 2.jpg" alt="" />
       <div
         class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center"
         style="background: rgba(24, 29, 56, .7);"
       >
         <div class="container">
-          <div class="row justify-content-start">
+          <div class="row justify-content-center">
             <div class="col-sm-10 col-lg-8">
               <h5 class="text-primary text-uppercase mb-3 animated slideInDown">
                 Embrace the Best of Blended Education
@@ -83,7 +101,32 @@
         </div>
       </div>
     </div>
-  </div>
+
+    <div slot="prev">
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#header-carousel"
+        data-bs-slide="prev"
+        on:click={() => handleSlideChangeClick(true)}
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true" />
+        <span class="visually-hidden">Previous</span>
+      </button>
+    </div>
+    <div slot="next">
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#header-carousel"
+        data-bs-slide="next"
+        on:click={() => handleSlideChangeClick(false)}
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true" />
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </Carousel>
 </div>
 
 <Services />
@@ -94,4 +137,9 @@
 <Team showAllTeamMembers={false} />
 
 <style>
+  .intro-item,
+  .intro-item img {
+    width: 100vw !important;
+    max-height: 700px;
+  }
 </style>
