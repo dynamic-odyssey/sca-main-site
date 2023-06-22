590ba830691bb404369d3481be60b9a0fb8fe2f7
RGertze <rgertze27@gmail.com>
2023-06-22 09:31:01 +0200

    Added Project Section

--- app/src/sections/team/team.svelte
+++ app/src/sections/team/team.svelte
@@ -2,304 +2,121 @@
   export let showAllTeamMembers: boolean = false;
 </script>
 
+<!-- Team Start -->
 <div class="container-xxl py-5">
   <div class="container">
     <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
       <h6 class="section-title bg-white text-center text-primary px-3">
-        Staff
+        Projects
       </h6>
-      <h1 class="mb-5">Expert Teachers</h1>
+      <h1 class="mb-5">School Projects</h1>
     </div>
     <div class="row g-4">
-      <div
-        class="col-lg-3 col-md-6 wow fadeInUp flex-grow-1"
-        data-wow-delay="0.1s"
-      >
+      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
         <div class="team-item bg-light">
           <div class="overflow-hidden">
-            <img class="img-fluid" src="img/teachers/Mr_P_W_Links.jpg" alt="" />
+            <img class="img-fluid" src="img/team-1.jpg" alt="" />
           </div>
           <div
             class="position-relative d-flex justify-content-center"
             style="margin-top: -23px;"
           >
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-facebook-f" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-twitter" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-instagram" /></a
-              >
-            </div>
+            <!--
+                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-instagram"></i></a>
+                            </div>
+                        -->
           </div>
           <div class="text-center p-4">
-            <h5 class="mb-0">Mr Links</h5>
-            <small>Principal</small>
+            <h5 class="mb-0">Soccer</h5>
+            <small>Sports</small>
           </div>
         </div>
       </div>
-      <div
-        class="col-lg-3 col-md-6 wow fadeInUp flex-grow-1"
-        data-wow-delay="0.3s"
-      >
+      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
         <div class="team-item bg-light">
           <div class="overflow-hidden">
-            <img class="img-fluid" src="img/teachers/Ms_D_Links.jpg" alt="" />
+            <img class="img-fluid" src="img/team-2.jpg" alt="" />
           </div>
           <div
             class="position-relative d-flex justify-content-center"
             style="margin-top: -23px;"
           >
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-facebook-f" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-twitter" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-instagram" /></a
-              >
-            </div>
+            <!--
+                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-instagram"></i></a>
+                            </div>
+                        -->
           </div>
           <div class="text-center p-4">
-            <h5 class="mb-0">Ms Links</h5>
-            <small>Primary School Teacher</small>
+            <h5 class="mb-0">Gardening</h5>
+            <small>Environment</small>
           </div>
         </div>
       </div>
-      <div
-        class="col-lg-3 col-md-6 wow fadeInUp flex-grow-1"
-        data-wow-delay="0.5s"
-      >
+      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
         <div class="team-item bg-light">
           <div class="overflow-hidden">
-            <img class="img-fluid" src="img/teachers/Mrs_Basson.jpg" alt="" />
+            <img class="img-fluid" src="img/team-3.jpg" alt="" />
           </div>
           <div
             class="position-relative d-flex justify-content-center"
             style="margin-top: -23px;"
           >
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-facebook-f" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-twitter" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-instagram" /></a
-              >
-            </div>
+            <!--
+                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-instagram"></i></a>
+                            </div>
+                        -->
           </div>
           <div class="text-center p-4">
-            <h5 class="mb-0">Mrs Basson</h5>
-            <small>Primary School Teacher</small>
+            <h5 class="mb-0">Science Fair</h5>
+            <small>STEM</small>
           </div>
         </div>
       </div>
-      <div
-        class="col-lg-3 col-md-6 wow fadeInUp flex-grow-1"
-        data-wow-delay="0.7s"
-      >
+      <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
         <div class="team-item bg-light">
           <div class="overflow-hidden">
-            <img class="img-fluid" src="img/teachers/Ms_Poulton.jpg" alt="" />
+            <img class="img-fluid" src="img/team-4.jpg" alt="" />
           </div>
           <div
             class="position-relative d-flex justify-content-center"
             style="margin-top: -23px;"
           >
-            <div class="bg-light d-flex justify-content-center pt-2 px-1">
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-facebook-f" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-twitter" /></a
-              >
-              <a class="btn btn-sm-square btn-primary mx-1" href=""
-                ><i class="fab fa-instagram" /></a
-              >
-            </div>
+            <!--
+                            <div class="bg-light d-flex justify-content-center pt-2 px-1">
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-sm-square btn-primary mx-1" href=""><i
+                                        class="fab fa-instagram"></i></a>
+                            </div>
+                        -->
           </div>
           <div class="text-center p-4">
-            <h5 class="mb-0">Ms Poulton</h5>
-            <small>High School Teacher</small>
+            <h5 class="mb-0">Spelling Bee</h5>
+            <small>Language</small>
           </div>
         </div>
       </div>
     </div>
-    {#if showAllTeamMembers}
-      <div class="row g-4">
-        <div
-          class="col-lg-3 col-md-6 wow fadeInUp flex-grow-1"
-          data-wow-delay="0.1s"
-        >
-          <div class="team-item bg-light">
-            <div class="overflow-hidden">
-              <img class="img-fluid" src="img/teachers/Mrs_Bussel.jpg" alt="" />
-            </div>
-            <div
-              class="position-relative d-flex justify-content-center"
-              style="margin-top: -23px;"
-            >
-              <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-facebook-f" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-twitter" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-instagram" /></a
-                >
-              </div>
-            </div>
-            <div class="text-center p-4">
-              <h5 class="mb-0">Mrs Bussel</h5>
-              <small>High School Teacher</small>
-            </div>
-          </div>
-        </div>
-        <div
-          class="col-lg-3 col-md-6 wow fadeInUp flex-grow-1"
-          data-wow-delay="0.3s"
-        >
-          <div class="team-item bg-light">
-            <div class="overflow-hidden">
-              <img
-                class="img-fluid"
-                src="img/teachers/Mrs_L_Abrahams.jpg"
-                alt=""
-              />
-            </div>
-            <div
-              class="position-relative d-flex justify-content-center"
-              style="margin-top: -23px;"
-            >
-              <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-facebook-f" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-twitter" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-instagram" /></a
-                >
-              </div>
-            </div>
-            <div class="text-center p-4">
-              <h5 class="mb-0">Mrs Abrahams</h5>
-              <small>Primary School Teacher</small>
-            </div>
-          </div>
-        </div>
-        <div
-          class="col-lg-3 col-md-6 wow fadeInUp flex-grow-1"
-          data-wow-delay="0.5s"
-        >
-          <div class="team-item bg-light">
-            <div class="overflow-hidden">
-              <img
-                class="img-fluid"
-                src="img/teachers/Mrs_R_Plaatjie.jpg"
-                alt=""
-              />
-            </div>
-            <div
-              class="position-relative d-flex justify-content-center"
-              style="margin-top: -23px;"
-            >
-              <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-facebook-f" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-twitter" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-instagram" /></a
-                >
-              </div>
-            </div>
-            <div class="text-center p-4">
-              <h5 class="mb-0">Mrs Plaatjie</h5>
-              <small>Pre-Primary School Teacher</small>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-          <div class="team-item bg-light">
-            <div class="overflow-hidden">
-              <img
-                class="img-fluid"
-                src="img/teachers/Ms_N_Plaatjie.jpg"
-                alt=""
-              />
-            </div>
-            <div
-              class="position-relative d-flex justify-content-center"
-              style="margin-top: -23px;"
-            >
-              <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-facebook-f" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-twitter" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-instagram" /></a
-                >
-              </div>
-            </div>
-            <div class="text-center p-4">
-              <h5 class="mb-0">Ms Plaatjie</h5>
-              <small>Primary School Teacher</small>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row g-4">
-        <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.7s">
-          <div class="team-item bg-light">
-            <div class="overflow-hidden">
-              <img
-                class="img-fluid"
-                src="img/teachers/Ms_Merceline_Hummel.jpg"
-                alt=""
-              />
-            </div>
-            <div
-              class="position-relative d-flex justify-content-center"
-              style="margin-top: -23px;"
-            >
-              <div class="bg-light d-flex justify-content-center pt-2 px-1">
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-facebook-f" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-twitter" /></a
-                >
-                <a class="btn btn-sm-square btn-primary mx-1" href=""
-                  ><i class="fab fa-instagram" /></a
-                >
-              </div>
-            </div>
-            <div class="text-center p-4">
-              <h5 class="mb-0">Ms Hummel</h5>
-              <small>Primary School Teacher</small>
-            </div>
-          </div>
-        </div>
-      </div>
-    {/if}
   </div>
 </div>
+
+<!-- Team End -->
 
 <style>
 </style>
