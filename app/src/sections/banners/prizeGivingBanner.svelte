17684065596277c31e1b478e7de44053202e3f08
RGertze <rgertze27@gmail.com>
2023-11-14 10:17:24 +0200

    Change in Contribution Pages to be more concise

--- app/src/sections/banners/prizeGivingBanner.svelte
+++ app/src/sections/banners/prizeGivingBanner.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { link } from "svelte-spa-router";
+  import { link } from "svelte-spa-router";
   // JavaScript or TypeScript code for this component
 </script>
 
@@ -9,16 +9,29 @@
       <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
         <div class="position-relative h-100">
           <!-- Replace with appropriate image -->
-          <img class="img-fluid position-absolute w-100 h-100" src="img/PGC.jpg" alt="Prize Giving Contributions" style="object-fit: contain;" />
+          <img
+            class="img-fluid position-absolute w-100 h-100"
+            src="img/PGC.jpg"
+            alt="Prize Giving Contributions"
+            style="object-fit: contain;"
+          />
         </div>
       </div>
       <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.6s">
-        <h6 class="section-title bg-white text-start text-primary pe-3">Prize Giving Contributions</h6>
+        <h6 class="section-title bg-white text-start text-primary pe-3">
+          Prize Giving Contributions
+        </h6>
         <h1 class="mb-4">Celebrating Student Achievements</h1>
         <p class="mb-4">
-          Join us in celebrating our students' achievements by contributing to the Prize Giving event. Your generosity helps us honor their hard work and dedication, inspiring them to continue striving for excellence.
+          Join us in a celebration of our students' achievements at our upcoming
+          Prize Giving event. This occasion is a testament to their hard work,
+          dedication, and pursuit of excellence. It's a moment to honor their
+          journey and inspire them for future endeavors. Let's come together to
+          applaud the milestones they have achieved and the goals they strive
+          towards. Our Prize Giving event is a proud reflection of our students'
+          commitment to learning and growth.
         </p>
-        <p class="banner-amount">Contributions Collected: <span>N$400</span></p>
+        <p class="banner-amount">Join us on 24 November 2023</p>
         <!-- Add call-to-action if needed -->
       </div>
     </div>
