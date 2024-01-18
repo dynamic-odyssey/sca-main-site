40413677f712575523b63a3f2f3fe70e5a10f98b
RGertze <rgertze27@gmail.com>
2024-01-18 15:18:44 +0200

    Updated Temporary Uniform Section

--- app/src/sections/uniform/uniform.svelte
+++ app/src/sections/uniform/uniform.svelte
@@ -1,22 +1,26 @@
 <script lang="ts">
   import { link } from "svelte-spa-router";
+
+  let currentImageIndex = 0;
+  const images = ["img/Uniform.png", "img/Uniform Address.png"]; // Replace with your actual image paths
+
+  function changeImage(next) {
+    if (next) {
+      currentImageIndex = (currentImageIndex + 1) % images.length;
+    } else {
+      currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    }
+  }
 </script>
 
 <div id="uniform-section" class="container-xxl py-5">
   <div class="container">
     <div class="row g-5">
-      <div
-        class="col-lg-6 wow fadeInUp"
-        data-wow-delay="0.1s"
-        style="min-height: 400px;"
-      >
+      <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
         <div class="position-relative h-100">
-          <img
-            class="img-fluid position-absolute w-100 h-100"
-            src="img/School Uniform.png"
-            alt=""
-            style="object-fit: contain;"
-          />
+          <img class="img-fluid position-absolute w-100 h-100" src={images[currentImageIndex]} alt="Uniform Image" style="object-fit: contain;" />
+          <button on:click={() => changeImage(false)} class="slideshow-button left">Previous</button>
+          <button on:click={() => changeImage(true)} class="slideshow-button right">Next</button>
         </div>
       </div>
       <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.6s">
@@ -40,17 +44,29 @@
           our administrative team. We appreciate your cooperation in maintaining
           our school's prestigious image.
         </p>
-        <a
-          class="btn btn-warning py-3 px-5 mt-2 me-3"
-          href="https://its.elearning-swakopca.edu.na/register">Enrol Now</a
-        >
-        <a class="btn btn-primary py-3 px-5 mt-2" use:link href="/contact"
-          >Get In Touch</a
-        >
+        <a class="btn btn-warning py-3 px-5 mt-2 me-3" href="https://its.elearning-swakopca.edu.na/register">Enrol Now</a>
+        <a class="btn btn-primary py-3 px-5 mt-2" use:link href="/contact">Get In Touch</a>
       </div>
     </div>
   </div>
 </div>
 
 <style>
+  .slideshow-button {
+    position: absolute;
+    bottom: 10px;
+    padding: 5px 10px;
+    color: white;
+    background-color: black;
+    border: none;
+    cursor: pointer;
+  }
+
+  .slideshow-button.left {
+    left: 10px;
+  }
+
+  .slideshow-button.right {
+    right: 10px;
+  }
 </style>
