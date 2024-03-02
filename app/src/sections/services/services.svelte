9321ff1c6f1aeea0e536994cc487fe5efd151b9c
RGertze <rgertze27@gmail.com>
2024-03-02 14:24:56 +0200

    Added Core Principles

--- app/src/sections/services/services.svelte
+++ app/src/sections/services/services.svelte
@@ -75,51 +75,63 @@
     </div>
   </div>
 {:else}
-  <div id="vision-section" class="container-xxl py-5">
+ <div id="vision-section" class="container-xxl py-5">
     <div class="container">
-      <div class="row g-4 justify-content-center">
-        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-          <div class="service-item text-center pt-3">
-            <div class="p-4">
-              <i class="fa fa-3x fa-eye text-primary mb-4" />
-              <h5 class="mb-3">Vision</h5>
-              <p>
-                1. Train up a child in the way he should go and when he is old he
-                will not depart from it. (Proverbs 22: 6). 2. We empower our
-                students with everything necessary and inspire one another to
-                take thoughtful action.
-              </p>
+        <div class="row g-4 justify-content-center">
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="service-item text-center pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-eye text-primary mb-4"></i>
+                        <h5 class="mb-3">Vision</h5>
+                        <p>
+                            1. Train up a child in the way he should go and when he is old he
+                            will not depart from it. (Proverbs 22: 6). 2. We empower our
+                            students with everything necessary and inspire one another to
+                            take thoughtful action.
+                        </p>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-          <div class="service-item text-center pt-3">
-            <div class="p-4">
-              <i class="fa fa-3x fa-rocket text-primary mb-4" />
-              <h5 class="mb-3">Mission</h5>
-              <p>
-                Our mission is to provide excellence in education through a
-                caring and diverse environment that encourages the holistic
-                development of each student as part of our mandate. We prepare
-                students with the means to succeed in a challenging world.
-              </p>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                <div class="service-item text-center pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-rocket text-primary mb-4"></i>
+                        <h5 class="mb-3">Mission</h5>
+                        <p>
+                            Our mission is to provide excellence in education through a
+                            caring and diverse environment that encourages the holistic
+                            development of each student as part of our mandate. We prepare
+                            students with the means to succeed in a challenging world.
+                        </p>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-          <div class="service-item text-center pt-3">
-            <div class="p-4">
-              <i class="fa fa-3x fa-globe text-primary mb-4" />
-              <h5 class="mb-3">Core Values</h5>
-              <p>
-                Swakopmund Christian Academy champions African heritage with a global outlook. Our motto is "Education is Victory for Africa." We utilize hybrid learning, blending in-person and digital instruction, ensuring flexibility, personalization, engagement, and adaptability for a well-rounded education.
-              </p>
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="service-item text-center pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-globe text-primary mb-4"></i>
+                        <h5 class="mb-3">Core Values</h5>
+                        <p>
+                            Swakopmund Christian Academy champions African heritage with a global outlook. Our motto is "Education is Victory for Africa." We utilize hybrid learning, blending in-person and digital instruction, ensuring flexibility, personalization, engagement, and adaptability for a well-rounded education.
+                        </p>
+                    </div>
+                </div>
             </div>
-          </div>
+            <!-- Core Principles Section -->
+            <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+                <div class="service-item text-center pt-3">
+                    <div class="p-4">
+                        <i class="fa fa-3x fa-anchor text-primary mb-4"></i> <!-- Change icon as needed -->
+                        <h5 class="mb-3">Core Principles</h5>
+                        <p>
+                            Swakopmund Christian Academy develops leaders who are steadfast in their faith and exhibit honesty, decency, respect, excellence, compassion, innovation, and open-mindedness. These are the core principles of our institution.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 {/if}
 
 <style>
